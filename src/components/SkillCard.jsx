e2afc0121781473ae7cca32f789192da851f1cad
Marcelo <marcelo011299@gmail.com>
2023-04-24 14:42:49 -0600

    SkillCard styled

--- src/components/SkillCard.jsx
+++ src/components/SkillCard.jsx
@@ -1,6 +1,30 @@
 import { Box, Typography } from "@mui/material";
 
-const SkillCard = () => {
-    return <div>SkillCard</div>;
+const SkillCard = ({ name, logo }) => {
+    return (
+        <Box
+            className="skill-card"
+            display="flex"
+            justifyContent="space-evenly"
+            flexDirection="column"
+            borderRadius="1rem"
+            my="3rem"
+            minHeight="13rem"
+            minWidth="12rem"
+            boxShadow="0px 10px 20px 0px #00000020"
+            bgcolor="rgba(255,255,255, 0)"
+            sx={{ mx: { sm: "2.2rem", xs: "0.2rem" } }}>
+            <img
+                width="112px"
+                height="112px"
+                className="card-img"
+                src={logo}
+                alt={name}
+            />
+            <Typography fontFamily="Syne" fontWeight="500" textAlign="center">
+                {name}
+            </Typography>
+        </Box>
+    );
 };
 export default SkillCard;
