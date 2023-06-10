bc55246d713b554e24906ac6d80de06f745c55df
Marcelo <marcelo011299@gmail.com>
2023-06-09 22:42:35 -0600

    projects hotfix

--- src/utils/projectsUrls.js
+++ src/utils/projectsUrls.js
@@ -1,6 +1,6 @@
 export const projects = [
     'https://gym-fitclub.netlify.app',
     'https://dev-toolbox.netlify.app',
-    'https://customshirt3d.netlify.app',
+    'https://marceloportobanco.dev',
     'https://task-director.netlify.app'
 ]
