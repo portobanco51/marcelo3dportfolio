de4368429ff370cd5bb9e18f5624e6ff07b440da
Marcelo <marcelo011299@gmail.com>
2023-09-24 19:02:39 -0600

    Title, exp 2023upd

--- src/components/canvas/Experience.jsx
+++ src/components/canvas/Experience.jsx
@@ -2,85 +2,74 @@ import { Center, Text3D } from "@react-three/drei";
 import { useThree } from "@react-three/fiber";
 
 const Experience = ({
-    margin = 1,
-    segments = 16,
-    height = 0.002,
-    spacing = 0.06,
-    bevelSize = 0.01,
-    thickness = 0.09,
+  margin = 1,
+  segments = 16,
+  height = 0.002,
+  spacing = 0.06,
+  bevelSize = 0.01,
+  thickness = 0.09,
 }) => {
-    const { width: w, height: h } = useThree((state) => state.viewport);
-    return (
-        <group position={[0, -26, -1.9]} scale={0.5}>
-            <Center right position={[-w / 2.4 + margin, -2, 0]}>
-                <Text3D
-                    curveSegments={segments}
-                    height={height}
-                    letterSpacing={spacing}
-                    bevelEnabled
-                    bevelSize={bevelSize}
-                    bevelThickness={thickness}
-                    size={h / 22}
-                    font={"/fonts/B612Mono.json"}>
-                    {`Front Desk\nTech Support\nAmazon, 2019`}
-                    <meshLambertMaterial
-                        emissive={"#919191"}
-                        color={"#ffffff"}
-                    />
-                </Text3D>
-            </Center>
-            <Center left position={[w / 2.4 - margin, -7.5, 0]}>
-                <Text3D
-                    curveSegments={segments}
-                    height={height}
-                    letterSpacing={spacing}
-                    bevelEnabled
-                    bevelSize={bevelSize}
-                    bevelThickness={thickness}
-                    size={h / 22}
-                    font={"/fonts/B612Mono.json"}>
-                    {`Transportation\n       Analyst` + `\n  Amazon, 2019`}
-                    <meshLambertMaterial
-                        emissive={"#919191"}
-                        color={"#ffffff"}
-                    />
-                </Text3D>
-            </Center>
-            <Center right position={[-w / 2.4 + margin, -13, 0]}>
-                <Text3D
-                    curveSegments={segments}
-                    height={height}
-                    letterSpacing={spacing}
-                    bevelEnabled
-                    bevelSize={bevelSize}
-                    bevelThickness={thickness}
-                    size={h / 22}
-                    font={"/fonts/B612Mono.json"}>
-                    {`Executive\nAdministrative\nAssistant\nIBM, 2021`}
-                    <meshLambertMaterial
-                        emissive={"#919191"}
-                        color={"#ffffff"}
-                    />
-                </Text3D>
-            </Center>
-            <Center left position={[w / 2.4 - margin, -18, 0]}>
-                <Text3D
-                    curveSegments={segments}
-                    height={height}
-                    letterSpacing={spacing}
-                    bevelEnabled
-                    bevelSize={bevelSize}
-                    bevelThickness={thickness}
-                    size={h / 22}
-                    font={"/fonts/B612Mono.json"}>
-                    {`          Web\n    Developer` + `\n Aztec P, now`}
-                    <meshLambertMaterial
-                        emissive={"#919191"}
-                        color={"#ffffff"}
-                    />
-                </Text3D>
-            </Center>
-        </group>
-    );
+  const { width: w, height: h } = useThree((state) => state.viewport);
+  return (
+    <group position={[0, -26, -1.9]} scale={0.5}>
+      <Center right position={[-w / 2.4 + margin, -2, 0]}>
+        <Text3D
+          curveSegments={segments}
+          height={height}
+          letterSpacing={spacing}
+          bevelEnabled
+          bevelSize={bevelSize}
+          bevelThickness={thickness}
+          size={h / 22}
+          font={"/fonts/B612Mono.json"}>
+          {`Transportation\nAnalyst\nAmazon, 2019`}
+          <meshLambertMaterial emissive={"#919191"} color={"#ffffff"} />
+        </Text3D>
+      </Center>
+      <Center left position={[w / 2.4 - margin, -7.5, 0]}>
+        <Text3D
+          curveSegments={segments}
+          height={height}
+          letterSpacing={spacing}
+          bevelEnabled
+          bevelSize={bevelSize}
+          bevelThickness={thickness}
+          size={h / 22}
+          font={"/fonts/B612Mono.json"}>
+          {`     Executive\nAdministrative\n     Assistant` +
+            `\n     IBM, 2021`}
+          <meshLambertMaterial emissive={"#919191"} color={"#ffffff"} />
+        </Text3D>
+      </Center>
+      <Center right position={[-w / 2.4 + margin, -13, 0]}>
+        <Text3D
+          curveSegments={segments}
+          height={height}
+          letterSpacing={spacing}
+          bevelEnabled
+          bevelSize={bevelSize}
+          bevelThickness={thickness}
+          size={h / 22}
+          font={"/fonts/B612Mono.json"}>
+          {`Web\nDeveloper\nAztec P, 2022`}
+          <meshLambertMaterial emissive={"#919191"} color={"#ffffff"} />
+        </Text3D>
+      </Center>
+      <Center left position={[w / 2.4 - margin, -18, 0]}>
+        <Text3D
+          curveSegments={segments}
+          height={height}
+          letterSpacing={spacing}
+          bevelEnabled
+          bevelSize={bevelSize}
+          bevelThickness={thickness}
+          size={h / 22}
+          font={"/fonts/B612Mono.json"}>
+          {`   Software\n   Engineer` + `\n Intel, now`}
+          <meshLambertMaterial emissive={"#919191"} color={"#ffffff"} />
+        </Text3D>
+      </Center>
+    </group>
+  );
 };
 export default Experience;
--- src/components/canvas/Hero3D.jsx
+++ src/components/canvas/Hero3D.jsx
@@ -1,15 +1,18 @@
 import { Rocket } from "../models";
 
 const Hero3D = () => {
-    const isMobile = window.innerWidth <= 600;
-    return (
-        <mesh
-            position={isMobile ? [-1.5, -2, 0] : [-0.8, -0.8, 0]}
-            scale={isMobile ? 0.2 : 0.4}
-            castShadow>
-            <Rocket />
-        </mesh>
-    );
+  const isMobile = window.innerWidth <= 600;
+  const isNarrow = window.innerWidth <= 380;
+  return (
+    <mesh
+      position={
+        isNarrow ? [-1.4, -2.7, 0] : isMobile ? [-1.5, -2, 0] : [-0.8, -0.8, 0]
+      }
+      scale={isNarrow ? 0.17 : isMobile ? 0.2 : 0.4}
+      castShadow>
+      <Rocket />
+    </mesh>
+  );
 };
 
 export default Hero3D;
