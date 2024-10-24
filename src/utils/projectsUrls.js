ca801bc51d124c479190cd69bab4f2f32aa35b11
Marcelo <76289550+portobanco51@users.noreply.github.com>
2024-10-24 01:11:25 -0600

    UPDATE: projects list
    * Updated and replaced outdated projects urls

--- src/utils/projectsUrls.js
+++ src/utils/projectsUrls.js
@@ -2,5 +2,5 @@ export const projects = [
     'https://github.com/portobanco51/ArrowKeysMenu',
     'https://customshirt3d.netlify.app',
     'https://marceloportobanco.netlify.app',
-    'https://gym-fitclub.netlify.app',
+    'https://github.com/portobanco51/AutomatedTesting',
 ]
